6e2181554cf3a0e79e6e644969e077227bb3bcdc
Ashik-ch <ashikch7117@gmail.com>
2025-10-10 00:24:58 +0530

    feat: add home component and routing
    - Implemented the home component with a responsive layout and background image.
    - Added routing for the home component as the default path.
    - Included buttons for navigating to inbound and outbound packages.
    - Created associated HTML and SCSS files for the home component.
    - Added multiple image assets for use in the application.
    - Configured Tailwind CSS for styling and updated global styles.
    - Added Tailwind CSS configuration file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./components/home/home').then(m => m.Home), },
+    // {
+    //     path: 'inbound',
+    //     children: [
+    //         { path: '', loadComponent: () => import('./components/inbound/inbound').then(m => m.Inbound), },
+    //         { path: ':state', loadComponent: () => import('./components/inbound/inbound-package/inbound-package').then(m => m.InboundPackage), }
+    //     ]
+    // },
+    // { path: 'outbound', loadComponent: () => import('./components/outbound/outbound/outbound').then(m => m.Outbound), },
+    // { path: 'contact', loadComponent: () => import('./components/pages/contact/contact').then(m => m.Contact), },
+];
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+// import { NgxTypewriterComponent } from '@omnedia/ngx-typewriter';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule,],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
