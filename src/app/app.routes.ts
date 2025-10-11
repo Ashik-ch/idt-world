69e96f4b718f5064685816108e258fcf21d76cd0
Ashik-ch <ashikch7117@gmail.com>
2025-10-11 14:02:53 +0530

    feat: comment out inbound package route in app routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./components/home/home').then(m => m.Home), title: 'Home' },
     { path: 'inbound', loadComponent: () => import('./components/inbound/inbound').then(m => m.Inbound), title: 'Inbound Tours' },
-    { path: 'inbound/:state', loadComponent: () => import('./components/inbound/inbound-package/inbound-package').then(m => m.InboundPackage), title: 'Inbound Package' },
+    // { path: 'inbound/:state', loadComponent: () => import('./components/inbound/inbound-package/inbound-package').then(m => m.InboundPackage), title: 'Inbound Package' },
     { path: 'outbound', loadComponent: () => import('./components/outbound/outbound').then(m => m.Outbound), title: 'Outbound Tours' },
     { path: 'contact', loadComponent: () => import('./components/pages/contact/contact').then(m => m.Contact), title: 'Contact Us' },
     { path: '**', redirectTo: '', pathMatch: 'full' }
