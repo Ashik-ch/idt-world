30e09d6d401beccbdfb756758fbdbbde13895cdf
Ashik-ch <ashikch7117@gmail.com>
2025-10-11 14:22:34 +0530

    feat: reorganize server routes for clarity and maintainability

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,36 @@
 // app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [{
-  path: 'inbound/:state',
-  renderMode: RenderMode.Prerender,
-  async getPrerenderParams() {
-    return [
-      { state: 'kerala' },
-      { state: 'delhi' },
-      { state: 'karnataka' }
-    ];
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'inbound',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'inbound/:state',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { state: 'kerala' },
+        { state: 'delhi' },
+        { state: 'karnataka' }
+      ];
+    }
+  },
+  {
+    path: 'outbound',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
-}
 ];
