61e2f61cdba2f8b35de98b1c71e2aca5859f07fc
Ashik-ch <ashikch7117@gmail.com>
2025-10-14 05:09:23 +0400

    slider

--- src/app/components/pages/service/service.ts
+++ src/app/components/pages/service/service.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { features, Services } from '../../../data/home.data';
 import { CommonModule } from '@angular/common';
 import * as AOS from 'aos';
 import { FormsModule } from '@angular/forms';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-service',
   imports: [CommonModule, FormsModule],
   templateUrl: './service.html',
-  styleUrl: './service.scss'
+  styleUrl: './service.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Service {
   features = features;
   services = Services;
   selectedService: any = null;
+
+  ngOnInit() {
+    register();
+  }
 
   openModal(service: any) {
     this.selectedService = service;
