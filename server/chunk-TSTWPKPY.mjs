94039964037436562207a278b6ca336aa3da3177
Ashik-ch <Ashik-ch@users.noreply.github.com>
2026-01-04 18:02:54 +0000

    deploy: ef3d3477c4cb6781bc46c0042ed7b692b39ba53e

--- server/chunk-TSTWPKPY.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{L as o,f as e}from"./chunk-UBAPKM7E.mjs";var r=class t{toggleChatSource=new e;toggleChat$=this.toggleChatSource.asObservable();triggerChat(){this.toggleChatSource.next()}static \u0275fac=function(a){return new(a||t)};static \u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"})};export{r as a};
