68fbb2050f9df32a066ce3c44aed19084e3b1f19
Ashik-ch <ashikch7117@gmail.com>
2026-01-04 14:57:15 +0400

    Auto-generated commit

---
+++ server/chunk-TSTWPKPY.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{L as o,f as e}from"./chunk-UBAPKM7E.mjs";var r=class t{toggleChatSource=new e;toggleChat$=this.toggleChatSource.asObservable();triggerChat(){this.toggleChatSource.next()}static \u0275fac=function(a){return new(a||t)};static \u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"})};export{r as a};
