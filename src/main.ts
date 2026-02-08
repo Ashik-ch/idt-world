1554dc3fa2e4e5d421f8651b6528e24d43cd28da
Ashik-ch <ashikch7117@gmail.com>
2026-02-08 14:06:18 +0400

    iange remvied

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { App } from './app/app';
 import * as AOS from 'aos';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  })
   .catch((err) => console.error(err));
-
-AOS.init({
-  duration: 1000,
-  easing: 'ease-in-out',
-  once: true,
-  mirror: false
-});
